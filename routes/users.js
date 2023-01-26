c6fb120a1fd58b7497700824950099c0a6654bdc
Elijah Monjardin <monjardinelijah120@gmail.com>
2023-01-26 09:28:13 +0800

    Added patches for post and comments

--- routes/users.js
+++ routes/users.js
@@ -20,7 +20,54 @@ router.get('/:id', getUser,(req,res)=>{
 router.get('/:id/posts', getUser,(req,res)=>{
     res.json(res.user.posts)
 })
+router.get('/:id/posts/:postIndex', getUser,(req,res)=>{
+    let postID=req.params.postIndex
+    console.log(postID)
+    res.json(res.user.posts[postID])
+})
+router.patch('/:id/posts/:postIndex/newComment', getUser,async(req,res)=>{
+    
+    let postID=req.params.postIndex
+    console.log(res.user.posts)
+    
+    const newComment={
+        name:req.body.name,
+        date:req.body.date,
+        content:req.body.content
+    }
+    /*
+    console.log(newComment)
+    console.log("Post ID: "+postID)
+    let overwriteComment= res.user.posts[postID].comments
+    console.log("Previous comment: ")
+    console.log(overwriteComment)
+    overwriteComment.unshift(newComment)
+    console.log("Updated comment: ")
+    console.log(overwriteComment)
 
+    res.user.posts[postID].comments=overwriteComment
+    console.log("TO PUSH:")
+    console.log(res.user.posts[postID].comments)
+    */
+    let currentPosts=res.user.posts;
+    console.log(currentPosts[postID].comments)
+    currentPosts[postID].comments.unshift(newComment)
+    console.log(currentPosts[postID].comments)
+    console.log("---------------------------------")
+    console.log(currentPosts)
+    
+    res.user.posts=currentPosts;
+
+    try {
+        res.user.markModified('posts')
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+        console.log(res.user.posts[postID].comments)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+    
+})
 router.post('/', async (req,res)=>{
     const user=new User({
         firstName:req.body.firstName,
@@ -83,9 +130,9 @@ router.patch('/:id/newPost', getUser, async (req,res)=>{
     }
     console.log(res.user.posts)
     let currentPosts=res.user.posts;
-    currentPosts.unshift(newPost);
+    currentPosts.push(newPost);
     
-    console.log("New comment "+currentPosts)
+    console.log("New Post "+currentPosts)
     res.user.posts=currentPosts;
 
     try {
@@ -96,6 +143,24 @@ router.patch('/:id/newPost', getUser, async (req,res)=>{
     }
 })
 //ADD POST
+router.delete('/:id/posts/:postIndex', getUser, async (req,res)=>{
+    
+    let postID=req.params.postIndex
+    
+    console.log(res.user.posts)
+    let currentPosts=res.user.posts;
+    currentPosts.splice(postID, 1); 
+    
+    res.user.posts=currentPosts;
+
+    try {
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+    } catch (err) {
+        res.status(400).json({message: err.message})
+    }
+})
+//DELETE POST
 router.delete('/:id', getUser, async(req,res)=>{
    try {
        await res.user.remove()
@@ -104,6 +169,8 @@ router.delete('/:id', getUser, async(req,res)=>{
        res.status(500).json({message: err.message})
    }
 })
+
+ 
 
 
 async function getUser(req,res,next){
